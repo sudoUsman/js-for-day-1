8aa1e22a0e41ef763d5f4a58df255bfeb65e9f58
sudoUsman <usmanishere9@gmail.com>
2024-09-30 21:14:24 -0700

    Update var.js

--- var.js
+++ var.js
@@ -1,1 +1,14 @@
-console.log("logo")
+
+const accountId = 144553
+let accountEmail = usmanishere@gmail.com
+var accountPassword = "12345"
+accountCity = "bahawalnagar"
+let accountState = ""
+accountId = 2
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState)}
+
+*/
+prefer not to usde var
+  because of issue in block scope and functional scope
+/*
